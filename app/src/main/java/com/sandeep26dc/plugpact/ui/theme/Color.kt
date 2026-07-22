6ff1d8d2d4bd91cf9d6df4f13af6f4bac94d171b
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 13:57:36 +0400

    Create Color.kt

---
+++ app/src/main/java/com/sandeep26dc/plugpact/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.sandeep26dc.plugpact.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Obsidian = Color(0xFF05070A)
+val DeepCharcoal = Color(0xFF0F141C)
+val ElectricCyan = Color(0xFF00F0FF)
+val EmeraldGreen = Color(0xFF00FF9D)
+val AmberWarning = Color(0xFFFFB800)
