2e2729e26775954a2edecc7d7a820ea4d2a0a286
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 15:04:07 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        // This is the core engine version
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
