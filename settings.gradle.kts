1b8b346cfa9a6ae28e3de6df15a3bbd0d6da5b86
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 15:22:01 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    // This mode ensures repositories are defined ONLY here
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
