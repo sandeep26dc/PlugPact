23a4abf7982b2aa6b5f51f01a1dbd9e42c9c0d84
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 15:16:34 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,14 @@
+// ROOT BUILD FILE - Strictly for Buildscript Classpath
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        // We are using 8.2.2 to match your JDK 17 setup
+        // Core Android & Kotlin build tools
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// NO "allprojects" block here. Repositories are managed in settings.gradle.kts
