0333286cda5c40f3189ee37096b42ac3d3a2d1c7
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 14:52:30 +0400

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// This tells Gradle which versions of the Android tools to use
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
