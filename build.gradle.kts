89392a64cde83fa5273b7872938fdfbe20cfd226
sandeep26dc <sandeep26dc@gmail.com>
2026-07-22 15:13:01 +0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
-// ROOT BUILD FILE - Optimized for PlugPact Build Engine
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        // Core Android & Kotlin build tools
+        // We are using 8.2.2 to match your JDK 17 setup
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
-// We removed the "allprojects" block because repositories are now 
-// managed in settings.gradle.kts to avoid conflicts.
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
